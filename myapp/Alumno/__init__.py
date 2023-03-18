0e0b155e4f940ef94afbed17c81438748ea7acb3
IDGS-802-20001500 <mauricioisraelfer@gmail.com>
2023-03-18 10:02:49 -0600

    04/03/2023

--- myapp/Alumno/__init__.py
+++
@@ -1,7 +0,0 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from config import DevelopmentConfig
-
-# app = Flask(__name__)
-# db = SQLAlchemy()
-# app.config.from_object(DevelopmentConfig)
--- myapp/app.py
+++ myapp/app.py
@@ -1,28 +1,20 @@
-from flask import Flask, redirect, render_template
-import forms
+import flask
 
-from flask import jsonify
-from config import DevelopmentConfig
-from flask_wtf.csrf import CSRFProtect
-from Alumno.routes import alumnos
-from Maestro.routes import maestros
-from models import db #ORM
+from Alumnos.routes import alumnos
+from Directivos.routes import directivos
+from Maestros.routes import maestros
 
-app=Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-csrf = CSRFProtect()
+app = flask.Flask(__name__)
+app.config["DEBUG"]=True
+
 
 @app.route("/", methods=["GET"])
 def home():
-    
-    return render_template("index.html")
+    return flask.jsonify({"principal":"HOME"})
 
 app.register_blueprint(alumnos)
+app.register_blueprint(directivos)
 app.register_blueprint(maestros)
 
-if __name__=="__main__":
-    csrf.init_app(app)
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-    app.run(port=3000)
+if __name__ =="__main__":
+    app.run()
